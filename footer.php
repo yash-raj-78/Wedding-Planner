36fc1e3696625c05f83c85cdeb71b4539aba58d9
Yash Raj <ryash5726@gmail.com>
2024-11-17 00:51:53 +0530

    Add

---
+++ footer.php
@@ -0,0 +1,45 @@
+<section class="footer">
+
+   <div class="box-container">
+
+      <div class="box">
+         <h3>quick links</h3>
+         <a href="home.php"> <i class="fas fa-angle-right"></i> home</a>
+         <a href="about.php"> <i class="fas fa-angle-right"></i> about</a>
+         <a href="portfolio.php"> <i class="fas fa-angle-right"></i> portfolio</a>
+         <a href="pricing.php"> <i class="fas fa-angle-right"></i> pricing</a>
+         <a href="contact.php"> <i class="fas fa-angle-right"></i> contact</a>
+      </div>
+
+      <div class="box">
+         <h3>extra links</h3>
+         <a href="#"> <i class="fas fa-angle-right"></i> plan wedding</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> our services</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> ask questions</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> terms of use</a>
+         <a href="#"> <i class="fas fa-angle-right"></i> privacy policy</a>
+      </div>
+
+      <div class="box">
+         <h3>contact info</h3>
+         <a href="#"> <i class="fas fa-phone"></i> +91-7857840512 </a>
+         <a href="#"> <i class="fas fa-phone"></i> +91-9939986789 </a>
+         <a href="#"> <i class="fas fa-envelope"></i> ryash5726@gmail.com </a>
+         <a href="#"> <i class="fas fa-envelope"></i> riya3213@gmail.com </a>
+         <a href="#"> <i class="fas fa-map"></i> Bihar, India - 851101 </a>
+      </div>
+
+      <div class="box">
+         <h3>follow us</h3>
+         <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i> facebook </a>
+         <a href="https://web.snapchat.com/"> <i class="fab fa-snapchat"></i> snapchat </a>
+         <a href="https://www.instagram.com/raj_yash_78578/"> <i class="fab fa-instagram"></i> instagram </a>
+         <a href="https://www.linkedin.com/feed/?trk=404_page"> <i class="fab fa-linkedin"></i> linkedin </a>
+         <a href="https://github.com/dashboard"> <i class="fab fa-github"></i> github </a>
+      </div>
+
+   </div>
+
+   <p class="credit"> created by <span>mr. Raj designer</span> | all rights reserved! </p>
+
+</section>
